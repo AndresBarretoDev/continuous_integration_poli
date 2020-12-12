8a33cb6ee80b5b3cfa1b86e2754284024748d7d3
SebastianBC <sebastianbrinezc@gmail.com>
2020-12-12 10:31:51 -0500

    Creación PUT method in aux_medico.js

--- public_html/API/src/routes/aux_medico.js
+++ public_html/API/src/routes/aux_medico.js
@@ -5,13 +5,27 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 
 router.post('/abuesoft/aux_medico/', (req, res) => {
-    const { nombre, apellido, tipo_doc, doc, tel, direccion, correo } = req.body;
-    const procedure_call = `CALL abuesoft.add_nurse(?, ?, ?, ?, ?, ?, ?);`;
-    mysqlConnection.query(procedure_call, [nombre, apellido, tipo_doc, doc, tel, direccion, correo], (err, rows, fields) => {
+    const { id, nombre, apellido, tipo_doc, doc, tel, direccion, correo } = req.body;
+    const procedure_call = `CALL abuesoft.add_nurse(?, ?, ?, ?, ?, ?, ?, ?);`;
+    mysqlConnection.query(procedure_call, [id, nombre, apellido, tipo_doc, doc, tel, direccion, correo], (err, rows, fields) => {
         if(!err){
-            res.json({status: "true"});
+            res.json({status: true});
         } else{
-            res.json({status: "false"});
+            res.json({status: false});
+            console.log(err);
+        }
+    });
+});
+
+router.put('/abuesoft/aux_medico/:id', (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, tipo_doc, doc, tel, direccion, correo } = req.body;
+    const procedure_call = `CALL abuesoft.add_nurse(?, ?, ?, ?, ?, ?, ?, ?);`;
+    mysqlConnection.query(procedure_call, [id, nombre, apellido, tipo_doc, doc, tel, direccion, correo], (err, rows, fields) => {
+        if(!err){
+            res.json({status: true});
+        }else{
+            res.json({status: false});
             console.log(err);
         }
     });
