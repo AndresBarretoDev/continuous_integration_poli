75fac058d2c3d1035d23bc0b5de7220095f6a2b9
SebastianBC <sebastianbrinezc@gmail.com>
2020-12-13 19:08:02 -0500

    Creación métodos PUT y GET en medicamento.

--- public_html/API/src/routes/medicamento.js
+++ public_html/API/src/routes/medicamento.js
@@ -16,4 +16,35 @@ router.post('/abuesoft/medicamento/', (req, res) => {
     });
 });
 
+router.put('/abuesoft/medicamento/:id', (req, res) => {
+    const {id} = req.params;
+    const {nombre, laboratorio, vencimiento, registro, dosis} = req.body;
+    const procedure_call = `CALL abuesoft.sp_AddEditMedicamento(?, ?, ?, ?, ?, ?);`;
+    mysqlConnection.query(procedure_call, [id, nombre, laboratorio, vencimiento, registro, dosis], (err, rows, fields) => {
+        if(!err){
+            res.json({status: true});
+        }else{
+            res.json({status: false});
+        }
+    });
+});
+
+router.get('/abuesoft/medicamento/', (req, res) => {
+    const query = `SELECT idmedicamento, 
+        nombre, 
+        laboratorio, 
+        fechaVencimiento, 
+        fechaRegistro, 
+        dosis 
+        FROM medicamento;`;
+    mysqlConnection.query(query, (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        }else{
+            res.json(err);
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
